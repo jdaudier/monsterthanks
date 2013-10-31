cd6d402e3093013c77c0026585c93b559e914ac3
Joanne Daudier <jdaudier@outlook.com>
2013-10-31 16:16:39 -0600

    This is when I gave up the side panel layout :cry:

--- models/cards.js
+++ models/cards.js
@@ -11,8 +11,8 @@ var Schema = mongoose.Schema;
 var monsterSchema = new Schema({
   top: Number,
   left: Number,
-  width: Number,
-  height: Number,
+  width: {type: Number, default: 100},
+  height: {type: Number, default: 100},
   speechBubble: {top : Number, left : Number}
 });
 
@@ -20,8 +20,123 @@ var monsterSchema = new Schema({
 
 var Monster = mongoose.model('Monster', monsterSchema);
 
+var monster1 = new Monster({
+  top: 20,
+  left: 20
+});
+
+var monster2 = new Monster({
+  top: 20,
+  left: 140
+});
+
+var monster3 = new Monster({
+  top: 144.8000030517578,
+  left: 20
+});
+
+var monster4 = new Monster({
+  top: 144.8000030517578,
+  left: 140
+});
+
+var monster5 = new Monster({
+  top: 269.6000061035156,
+  left: 20
+});
+
+var monster6 = new Monster({
+  top: 269.6000061035156,
+  left: 140
+});
+
+var monster7 = new Monster({
+  top: 394.3999938964844,
+  left: 20
+});
+
+var monster8 = new Monster({
+  top: 394.3999938964844,
+  left: 140
+});
+
+var monster9 = new Monster({
+  top: 519.2000122070312,
+  left: 20
+});
+
+var monster10 = new Monster({
+  top: 519.2000122070312,
+  left: 140
+});
+
+var monster11 = new Monster({
+  top: 644,
+  left: 20
+});
+
+var monster12 = new Monster({
+  top: 644,
+  left: 140
+});
+
+var monster13 = new Monster({
+  top: 768.7999877929688,
+  left: 20
+});
+
+var monster14 = new Monster({
+  top: 768.7999877929688,
+  left: 140
+});
+
+var monster15 = new Monster({
+  top: 893.6000366210938,
+  left: 20
+});
+
+var monster16 = new Monster({
+  top: 893.6000366210938,
+  left: 140
+});
+
+var monster17 = new Monster({
+  top: 1018.4000244140625,
+  left: 20
+});
+
+var monster18 = new Monster({
+  top: 1018.4000244140625,
+  left: 140
+});
+
+var monster19 = new Monster({
+  top: 1143.2000732421875,
+  left: 20
+});
+
+var monster20 = new Monster({
+  top: 1143.2000732421875,
+  left: 140
+});
+
+var monster21 = new Monster({
+  top: 1268,
+  left: 20
+});
+
+var monster22 = new Monster({
+  top: 1268,
+  left: 140
+});
+
+var monster23 = new Monster({
+  top: 1392.800048828125,
+  left: 20
+});
+
 var Card = mongoose.model('Card', {
-  monsters: [monsterSchema],
+  monsters: {type: [monsterSchema], default: [monster1, monster2, monster3, monster4, monster5, monster6, monster7, monster8, monster9, monster10, monster11, monster12, monster13, monster14, monster15,monster16, monster17, monster18, monster19, monster20, monster21, monster22, monster23]},
   background: String
 });
 
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -8,23 +8,37 @@ $(function domReady() {
 
   $('#open-closet').sidr();
 
-  socket.on('connect', function(){
-    // Gets position of all monsters after opening the closet
-    $('#open-closet').click(function(){
-      $('.draggable').each(function(){
-        var monstersPosition = $(this).position();
-        // 23 objects
-        console.log(monstersPosition);
-        socket.emit('monstersPosition', monstersPosition);
-      });
-    });
-  });
+  // $('#open-closet').click(function(){
+  //   $('.draggable').each(function(){
+  //     var monstersPosition = $(this).position();
+      // 23 objects
+      // console.log(monstersPosition);
+  //   });
+  // });
+
+  // $('#open-closet').click(function(){
+  //   $('.monster').each(function(){
+  //     var width = $(this).width();
+  //     var height = $(this).height();
+  //     console.log("width: ", width);
+  //     console.log("height: ", height);
+  //   });
+  // });
+
+  // socket.on('connect', function(){
+    // socket.emit('monstersPosition', monstersPosition);
+  // });
 
   // Gets position of a monster after dragstop
   $('.draggable').draggable({
     scroll: true,
     // appendTo: 'body',
-    // containment: 'window',
+    containment: 'window',
+    start: function( event, ui ) {
+      // var $sidr = $(this).parents('.sidr');
+
+      // $sidr.css("overflow-y", "visible");
+    },
     stop: function( event, ui ) {
       var location = $(this).position();
       console.log(location);
@@ -32,8 +46,13 @@ $(function domReady() {
     }
   });
 
-  // Gets size of monster
+  // Gets size of monster after resizing
   $('.monster').resizable({
+    start: function( event, ui ) {
+      var $monsterContainer = $(this).parents('.monster-container');
+      // $monsterContainer.empty();
+      // $monsterContainer.css("height", "100");
+    },
     stop: function( event, ui ) {
       var width = $(this).width();
       var height = $(this).height();
@@ -42,8 +61,6 @@ $(function domReady() {
     }
   });
 
-
-  // $('.dropzone').droppable({ accept: '.draggable' });
 
 
 
